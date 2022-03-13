8e66966d8c18b5035ef349ace489397046cfa6de
Karenkoech <karenkoech3@gmail.com>
2022-03-13 18:01:22 -0400

    give functionality to the form

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
-    $("button").click(function(textDes){
-      $(".description").show();
+    $("design-icon#icon1").click(function(){
+      $("#icon1,#para1").toggle();
     });
-  });
-  $(document).ready(function(){
-    $("button").click(function(textDes){
-      $(".description").show();
     });
-  });
+    //   $().toggle();
+
+    // $("button#btn2").click(function(textDes){
+    //     $(".dev_description").show();});
+
+    // $("button#btn3").click(function(textDes){
+    //     $(".product_des").show();
+    //     e.preventDefault();
+
+    });
+    
   
+  
+  $(document).ready(function(){
+    $("button").click(function(){
+      $(".message-form").show();
+      var message=document.getElementById("message").value
+      var name=document.getElementById("name").value
+      var email=document.getElementById("email").value
+
+      alert( `Hey ` + name + ` we have received your message.Thank you reaching out to us.`);
+    });
+  });
+  //Form validation
+  if 
+
+
