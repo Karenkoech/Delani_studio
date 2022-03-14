8f286223e1666f2770a7d5b9933c8c324bc056cb
Karenkoech <karenkoech3@gmail.com>
2022-03-14 05:04:15 -0400

    input functions from html

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
+// What-we-do function
 $(document).ready(function(){
-    $("design-icon#icon1").click(function(){
-      $("#icon1,#para1").toggle();
-    });
-    });
-    //   $().toggle();
+   $("#icon1,#para1").hover(function(){
+       $("#icon1,#para1").toggle()
+   })
+   $("#icon2,#para2").click(function(){
+    $("#icon2,#para2").toggle()
+})
+$("#icon3,#para3").click(function(){
+    $("#icon3,#para3").toggle()
+})
 
-    // $("button#btn2").click(function(textDes){
-    //     $(".dev_description").show();});
+})
+//  Contact us function
+$("button").click(function(){
+        var message=document.getElementById("message").value
+        var userName=document.getElementById("name").value
+        var email=document.getElementById("email").value
 
-    // $("button#btn3").click(function(textDes){
-    //     $(".product_des").show();
-    //     e.preventDefault();
-
-    });
     
-  
-  
-  $(document).ready(function(){
-    $("button").click(function(){
-      $(".message-form").show();
-      var message=document.getElementById("message").value
-      var name=document.getElementById("name").value
-      var email=document.getElementById("email").value
-
-      alert( `Hey ` + name + ` we have received your message.Thank you reaching out to us.`);
-    });
-  });
   //Form validation
-  if 
-
+  if (userName=="" || message=="" ||email==""){
+      alert( "check if you've entered userName , email and message")
+    }
+  else {
+    alert( `Hey ` + userName + ` we have received your message.Thank you reaching out to us.`)
+  
+   };
+})
 
